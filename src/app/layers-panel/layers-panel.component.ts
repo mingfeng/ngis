23bab74947aaf84dbff3bbb4ceb4272916286a76
Mingfeng Yu <mingfeng.yu@anders.fi>
2018-04-16 17:47:42 +0300

    Show layer service layers for layer panel

--- src/app/layers-panel/layers-panel.component.ts
+++ src/app/layers-panel/layers-panel.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LayerService } from '../services/layer.service';
+import { Layer } from '../shared/layer';
+
 @Component({
   selector: 'app-layers-panel',
   templateUrl: './layers-panel.component.html',
   styleUrls: ['./layers-panel.component.scss']
 })
 export class LayersPanelComponent implements OnInit {
+  basemapLayers: Layer[] = [];
+  overlayLayers: Layer[] = [];
 
-  constructor() { }
+  constructor(private layerService: LayerService) { }
 
   ngOnInit() {
+    this.getLayers();
   }
 
+  getLayers() {
+    this.layerService.getLayers().subscribe((layers) => {
+      layers.forEach((layer) => {
+        if (layer.isBasemap) {
+          this.basemapLayers.push(layer);
+        } else {
+          this.overlayLayers.push(layer);
+        }
+      });
+    });
+  }
 }
