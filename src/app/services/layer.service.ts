24840790991db8d5211851fa4c1924b8bbd5163f
Mingfeng Yu <mingfeng.yu@anders.fi>
2018-04-20 23:17:57 +0300

    Use mock layers for layer service

--- src/app/services/layer.service.ts
+++ src/app/services/layer.service.ts
@@ -3,28 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { Layer, LayerType } from '../shared/layer';
-
-const LAYERS = [{
-  type: LayerType.TILE,
-  identifier: 'osm',
-  name: 'OpenStreetMap',
-  url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  isBasemap: true
-}, {
-  type: LayerType.TILE,
-  identifier: 'wkm',
-  name: 'Wikimedia Maps',
-  url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
-  isBasemap: true
-}, {
-  type: LayerType.WMS,
-  identifier: 'countries',
-  name: 'Countries',
-  url: 'http://disc1.sci.gsfc.nasa.gov/daac-bin/wms_airsnrt?layer=AIRS_SO2_A&',
-  params: {'LAYERS': 'countries'},
-  projection: 'EPSG:4326',
-  isBasemap: false
-}];
+import { LAYERS } from '../shared/mock';
 
 @Injectable()
 export class LayerService {
