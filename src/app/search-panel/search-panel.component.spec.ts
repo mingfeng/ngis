8cda55bdcda5333840beb3dde75a324ae3d9d8b7
Mingfeng Yu <mingfeng.yu@anders.fi>
2018-05-12 19:59:49 +0300

    Add tests for seach panel search and select methods

--- src/app/search-panel/search-panel.component.spec.ts
+++ src/app/search-panel/search-panel.component.spec.ts
@@ -1,17 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchPanelComponent } from './search-panel.component';
+import { MapService } from '../services/map.service';
 
 describe('SearchPanelComponent', () => {
   let component: SearchPanelComponent;
   let fixture: ComponentFixture<SearchPanelComponent>;
+  let mapServiceSpy: jasmine.SpyObj<MapService>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
+    mapServiceSpy = jasmine.createSpyObj('MapService', [
+      'searchByText',
+      'selectSearchItem',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [ SearchPanelComponent ]
+      declarations: [ SearchPanelComponent ],
+      providers: [
+        { provide: MapService, useValue: mapServiceSpy }
+      ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchPanelComponent);
@@ -21,5 +31,16 @@ describe('SearchPanelComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#search should call map service spy method searchByText', () => {
+    component.search('text');
+    expect(mapServiceSpy.searchByText).toHaveBeenCalledWith('text');
+  });
+
+  it('#select should call map service spy method selectSearchItem', () => {
+    const searchItem = { id: 'ABC', name: 'ABC name' };
+    component.select(searchItem);
+    expect(mapServiceSpy.selectSearchItem).toHaveBeenCalledWith(searchItem);
   });
 });
