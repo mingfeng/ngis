920dbb2cbcf25a553c36fc649689e4c9d14e608b
Mingfeng Yu <mingfeng.yu@anders.fi>
2018-05-12 22:22:45 +0300

    Show search panel when search result get next value

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MapService } from '../services/map.service';
+
+declare var $: any;
+
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.scss']
 })
 export class SidePanelComponent implements OnInit {
+  activeTab = 'layers';
 
-  constructor() { }
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
+    this.mapService.searchResult.subscribe({
+      next: () => $('#side-panel-tab a[href="#search"]').tab('show')
+    });
   }
 
 }
