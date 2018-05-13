306edffbcb76dd592c4774239879796be3bf9047
Mingfeng Yu <mingfeng.yu@anders.fi>
2018-05-13 14:00:32 +0300

    Add tests for side-panel

--- src/app/search-panel/search-panel.component.spec.ts
+++ src/app/search-panel/search-panel.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchPanelComponent } from './search-panel.component';
 import { MapService } from '../services/map.service';
-import { Subject } from 'rxjs';
 import { SearchItem } from '../shared/search-item';
 
 describe('SearchPanelComponent', () => {
--- src/app/side-panel/side-panel.component.spec.ts
+++ src/app/side-panel/side-panel.component.spec.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MapService } from '../services/map.service';
 import { SidePanelComponent } from './side-panel.component';
 
 describe('SidePanelComponent', () => {
   let component: SidePanelComponent;
   let fixture: ComponentFixture<SidePanelComponent>;
+  let mapService: MapService;
 
   @Component({selector: 'app-search-panel', template: ''})
   class SearchPanelStubComponent {}
@@ -14,11 +16,16 @@ describe('SidePanelComponent', () => {
   class LayersPanelStubComponent {}
 
   beforeEach(async(() => {
+    mapService = new MapService(undefined, undefined);
+
     TestBed.configureTestingModule({
       declarations: [
         SidePanelComponent,
         SearchPanelStubComponent,
         LayersPanelStubComponent
+      ],
+      providers: [
+        { provide: MapService, useValue: mapService }
       ]
     })
     .compileComponents();
@@ -32,5 +39,22 @@ describe('SidePanelComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should activate layers tab on start', () => {
+    const layersTab = fixture.nativeElement.querySelector('a[href="#layers"]');
+    expect(layersTab.classList.contains('active')).toBeTruthy();
+    const layersTabContent = fixture.nativeElement.querySelector('#layers');
+    expect(layersTabContent.classList.contains('show')).toBeTruthy();
+    expect(layersTabContent.classList.contains('active')).toBeTruthy();
+  });
+
+  it('should activate search tab when map service search result is updated', () => {
+    mapService.searchResult.next([]);
+    const searchTab = fixture.nativeElement.querySelector('a[href="#search"]');
+    expect(searchTab.classList.contains('active')).toBeTruthy();
+    const searchTabContent = fixture.nativeElement.querySelector('#search');
+    expect(searchTabContent.classList.contains('show')).toBeTruthy();
+    expect(searchTabContent.classList.contains('active')).toBeTruthy();
   });
 });
