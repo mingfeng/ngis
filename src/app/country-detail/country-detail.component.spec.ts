cc6499b743bba9c5f79046b8bc0da2921848bf6b
Mingfeng Yu <mingfeng.yu@anders.fi>
2018-05-14 23:40:27 +0300

    Update country detail component with layout service

--- src/app/country-detail/country-detail.component.spec.ts
+++ src/app/country-detail/country-detail.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CountryDetailComponent } from './country-detail.component';
+import { LayoutService } from '../services/layout.service';
 
 describe('CountryDetailComponent', () => {
   let component: CountryDetailComponent;
   let fixture: ComponentFixture<CountryDetailComponent>;
 
+  const layoutServiceSpy = jasmine.createSpyObj('LayoutService', ['hideDetailPanel']);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CountryDetailComponent ]
+      declarations: [ CountryDetailComponent ],
+      providers: [
+        { provide: LayoutService, useValue: layoutServiceSpy }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +26,10 @@ describe('CountryDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#hideDetailPanel should call spy layout service hideDetailPanel', () => {
+    component.hideDetailPanel();
+    expect(layoutServiceSpy.hideDetailPanel).toHaveBeenCalled();
   });
 });
--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from '../services/layout.service';
 
 @Component({
   selector: 'app-country-detail',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private layoutService: LayoutService) { }
 
   ngOnInit() {
+  }
+
+  hideDetailPanel() {
+    this.layoutService.hideDetailPanel();
   }
 
 }
